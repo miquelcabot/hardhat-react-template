7145bcf2fd52b2241a52b0484ecc3479d3d1816d
miquelcabot <miquel.cabot@gmail.com>
2021-04-07 11:26:26 +0200

    Add typechain library and tsconfig file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,10 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+ import 'hardhat-typechain'
+ import '@nomiclabs/hardhat-ethers'
+ import '@nomiclabs/hardhat-waffle'
+ 
 module.exports = {
   solidity: "0.7.3",
 };
