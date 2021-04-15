295b9c8ef4ca147e40321a64ba45fb0fcf55b3bd
miquelcabot <miquel.cabot@gmail.com>
2021-04-15 19:12:25 +0200

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,20 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import DeliveryNew from './pages/DeliveryNew';
 import DeliveryShow from './pages/DeliveryShow';
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
-        <div>
+        <Container>
           <Header />
           <Switch>
               <Route exact path='/' component={Home}/>
               <Route exact path='/deliveries/new' component={DeliveryNew}/>
               <Route exact path='/deliveries/:address' component={DeliveryShow}/>
           </Switch>
-        </div>
+        </Container>
     );
   }
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 class Header extends Component {
   render() {
     return (
-      <p>Header</p>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand as={Link} to='/'>eDelivery with Ethereum</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to='/'>Deliveries</Nav.Link>
+          <Nav.Link as={Link} to='/deliveries/new'>+</Nav.Link>
+        </Nav>
+      </Navbar>
     );
   }
 }
