895a636a3cf136779fe19177768250fbf2d898a5
miquelcabot <miquel.cabot@gmail.com>
2021-04-16 17:57:21 +0200

    Add web3-react library

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createWeb3ReactRoot, Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+
+const Web3ProviderNetwork = createWeb3ReactRoot('NETWORK')
+
+function getLibrary(provider: any): Web3Provider {
+  const library = new Web3Provider(provider, 'any')
+  library.pollingInterval = 15000
+  return library
+}
 
 ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+  <Web3ReactProvider getLibrary={getLibrary}>
+      <Web3ProviderNetwork getLibrary={getLibrary}>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </Web3ProviderNetwork>
+  </Web3ReactProvider>,
   document.getElementById('root')
 );
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
+// import { useWeb3React } from '@web3-react/core'
+
+// const { active, account, connector, activate, error } = useWeb3React()
+
+/*function Web3 () {
+  const web3React = useWeb3React()
+  return web3React
+}*/
 
 class Home extends Component {
+  state = {
+    loadingPage: true,
+    loading: false,
+    errorMessage: ''
+  };
+
+  componentDidMount = async () => {
+    //await window.ethereum.enable();
+    //const provider = new ethers.providers.Web3Provider(window.ethereum);
+  }
+
   render() {
     return (
       <p>Home</p>
