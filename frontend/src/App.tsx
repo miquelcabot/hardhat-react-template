0cd84ab39339fc761e04e0ef692fe241a4f17ee8
miquelcabot <miquel.cabot@gmail.com>
2021-04-19 23:12:12 +0200

    Add material-ui library

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,16 @@ import { Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Edit from './pages/Edit';
-import Container from 'react-bootstrap/Container';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
-    <Container>
+    <>
       <Header />
       <Switch>
         <Route exact path='/' component={Home}/>
         <Route exact path='/edit' component={Edit}/>
       </Switch>
-    </Container>
+    </>
   )
 }
 
