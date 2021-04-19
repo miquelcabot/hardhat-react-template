767d3c5e8fae919cfa74731cd11ceaa633aee43f
miquelcabot <miquel.cabot@gmail.com>
2021-04-19 23:54:01 +0200

    Add main container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,34 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Edit from './pages/Edit';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2),
+  },
+}));
 
 const App = () => {
+  const classes = useStyles();
+
   return (
     <>
       <Header />
-      <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route exact path='/edit' component={Edit}/>
-      </Switch>
+      <Container component="main" className={classes.main} maxWidth="sm">
+        <Box>
+          <Typography variant="body1">
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/edit' component={Edit}/>
+            </Switch>
+          </Typography>
+        </Box>
+      </Container>
       <Footer />
     </>
   )
