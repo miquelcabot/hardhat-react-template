0e0397c65c0d8a6caf6c177dff8f02db941c48a3
miquelcabot <miquel.cabot@gmail.com>
2021-04-21 23:09:58 +0200

    Change web3status button deppending on status

--- frontend/src/components/Web3Status.tsx
+++ frontend/src/components/Web3Status.tsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import { Button } from '@material-ui/core'
-import { useWeb3React } from '@web3-react/core';
+import React from 'react';
+import { Button } from '@material-ui/core';
+import { UnsupportedChainIdError, useWeb3React } from '@web3-react/core';
+import { shortenAddress } from '../utils'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  connected: {
+    color: 'aqua',
+  },
+}));
 
 function Web3StatusInner() {
+  const classes = useStyles();
+
   const { account, error } = useWeb3React()
 
   if (account) {
     return (
-      <>
-        Account: {account}
-      </>
+      <Button href="#" color="default" variant="outlined" className={classes.connected}>
+        {shortenAddress(account)}
+      </Button>
     )
   } else if (error) {
     return (
-      <>
-        Error: {error}
-      </>
+      <Button href="#" color="secondary" variant="outlined">
+        {error instanceof UnsupportedChainIdError ? 'Wrong Network' : 'Error'}
+      </Button>
     )
   } else {
     return (
@@ -27,6 +37,13 @@ function Web3StatusInner() {
 }
 
 export default function Web3Status() {
+  /*const { active } = useWeb3React()
+
+  const contextNetwork = useWeb3React(NetworkContextName)
+
+  if (!contextNetwork.active && !active) {
+    return null
+  }*/
 
   return (
     <>
